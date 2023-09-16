1970edf6bc1341aab45f9c1e32b799d5d849fe9b
ivanmrosa <ivanmrosa@gmail.com>
2023-09-16 18:08:15 -0300

    env

--- env.py
+++ env.py
@@ -1,17 +1,18 @@
-import os, sys
-from  typing import TextIO
+import os
+import sys
+from typing import TextIO
 
 
 baseDir = os.path.dirname(__file__)
-class Env:    
-    __File : TextIO
+
+
+class Env:
     def __init__(self) -> None:
-        if not self.__File:
-            with open(os.path.join(baseDir, '.env')) as f:
-                lines = f.readlines()
-                for line in lines:
-                    keyValue = line.split("=")
-                    self.__setattr__(keyValue[0], keyValue[1])
-    
+        with open(os.path.join(baseDir, '.env')) as f:
+            lines = f.read().split('\n')
+            for line in lines:
+                keyValue = line.split("=")
+                self.__setattr__(keyValue[0], keyValue[1])
+
     def getEnvValue(self, name: str) -> str:
         return self.__getattribute__(name)
